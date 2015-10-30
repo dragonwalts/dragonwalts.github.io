7b798513faa34d18d3eb309b2a7a6d25f8126df2
dragonwalts <dragonwalts@rocketmail.com>
2015-10-30 16:23:35 +0800

    initial setup

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var ans =Math.floor (100* Math.Random()+1);
+
+function process(){
+	var input = $("#input").val();
+
+	if(input==ans){
+	$(response).html ("Correct!Congratulations!);
+	}else if (input<ans){
+	($response)html ("Wrong, it is higher.");
+	}else{
+	$(#response).html ("Wrong, it is lower.");
+	}
+}
